877de55447acf5abb0d45c11e30c83cfa1bc1239
Jaime Vargas <jvarbae782@g.educaand.es>
2025-10-28 12:22:15 +0100

    ejer1 prac5 hecho y comprobado

--- PrimerTrimestre/aplicacion/Practica5/index.php
+++ PrimerTrimestre/aplicacion/Practica5/index.php
@@ -34,7 +34,41 @@ function cuerpo()
         echo "<p>Número fuera de rango, corregido a: $var</p>";
     }
 
-    echo "<h3>Prueba de metodo validaReal de filter</h3>"
+    $var = 25.2;
+    echo "<h3>Prueba de metodo validaReal de filter</h3>";
+    \VALFILTER\validaReal($var,1,26,1);
+    echo "<p>$var</p>";
+
+    $var = "jaime@2daw.com";
+    echo "<h3>Prueba de metodo validaEmail de filter</h3>";
+    \VALFILTER\validaEmail($var,"aaa@gg.es");
+    echo "<p>$var</p>";
+
+    $var = "666778890";
+    echo "<h3>Prueba de metodo validaExpresion de filter</h3>";
+    \VALFILTER\validaExpresion($var,"/^[0-9]{9}$/","Número mal escrito");
+    echo "<p>$var</p>";
+
+    $var = "1/8/2006";
+    echo "<h3>Prueba de metodo validaFecha de filter</h3>";
+    \VALFILTER\validaFecha($var,"fecha no valida");
+    echo "<p>$var</p>";
+
+    $var = "21:32:18";
+    echo "<h3>Prueba de metodo validaHora de filter</h3>";
+    \VALFILTER\validaHora($var,"Hora no válida");
+    echo "<p>$var</p>";
+
+    $var = "hola";
+    $opciones = ["hola"=>1,"adios"=>2];
+    echo "<h3>Prueba de metodo validaRango de filter</h3>";
+    \VALFILTER\validaRango($var,$opciones,1);
+    echo "<p>$var</p>";
+
+    $var = "holaholahola";
+    echo "<h3>Prueba de metodo validaCadena de filter</h3>";
+    \VALFILTER\validaCadena($var,25,"Cadena demasiado larga");
+    echo "<p>$var</p>";
     ?>
 <?php
 }
